7fcd83b822e67640a4ff5e10c54cc1f400449b94
ktyudha <ktyudha@gmail.com>
2024-03-17 01:57:58 +0700

    renew controller get all ponds

--- src/controllers/ponds/get-all-noquery.ponds.controllers.ts
+++ src/controllers/ponds/get-all-noquery.ponds.controllers.ts
@@ -1,51 +1,154 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
-import { Ponds, Users } from '../../models';
-import { extractToken } from '../../utils';
+import { City, Ponds, Province, Users } from '../../models';
+import { extractToken, NOT_ARCHIVED } from '../../utils';
+import { objectIdValidator } from '../../validators';
 import message from '../../views/message';
 
 async function getAllNoQueryPonds(req: Request, res: Response) {
-  const userId = extractToken(req.headers.authorization, false).result;
-  if (userId) {
-    const isUserExist = await Users.find();
-    if (!isUserExist) {
-      return res.status(404).send(
+  const limit: number = parseInt(req.query.limit as string) || 1;
+  const page: number = parseInt(req.query.page as string) || 1;
+  const pondsName: string = (req.query.pondsName as string) || '';
+  const provinceId: string = (req.query.provinceId as string) || '';
+  const cityId: string = (req.query.cityId as string) || '';
+
+  const userId = extractToken(req.headers.authorization, false).result._id;
+  const isIdValid = objectIdValidator(userId as string, 'User', true);
+  if (isIdValid.error) {
+    return res.status(401).send(
+      message({
+        statusCode: 401,
+        data: req.body,
+        message: 'Token tidak valid!'
+      })
+    );
+  }
+
+  const isUserExist = await Users.findById(userId);
+  if (!isUserExist) {
+    return res.status(404).send(
+      message({
+        statusCode: 404,
+        data: req.body,
+        message: 'Pengguna dengan token yang diberikan tidak ditemukan!'
+      })
+    );
+  }
+
+  if (provinceId) {
+    // const isProvIdVal = objectIdValidator(provinceId as string, 'User', true);
+    // if (isProvIdVal.error) {
+    //   return res.status(400).send(
+    //     message({
+    //       statusCode: 400,
+    //       data: req.body,
+    //       message: 'ID provinsi tidak valid!'
+    //     })
+    //   );
+    // }
+
+    const isProvinceExist = await Province.findById(provinceId);
+    if (!isProvinceExist) {
+      return res.status(400).send(
         message({
-          statusCode: 404,
-          message: 'User is not found',
-          data: req.body
+          statusCode: 400,
+          data: req.body,
+          message: 'Propinsi dengan ID yang diberikan tidak ditemukan!'
         })
       );
     }
 
-    const getAllPonds = await Ponds.find()
-      .populate('userId')
-      .populate({ path: 'cityId', populate: { path: 'provinceId' } });
-    if (!getAllPonds) {
+    City.find({ provinceId, ...NOT_ARCHIVED }, { _id: 1 }, function (err, docs: any) {
+      const ids = docs.map(function (doc: any) {
+        return doc._id;
+      });
+
+      return Ponds.find(
+        { cityId: { $in: ids }, ...NOT_ARCHIVED, pondsName: { $regex: new RegExp(pondsName, 'i') } },
+        function (err: any, docs: any) {
+          return res.send(
+            message({
+              statusCode: 200,
+              message: 'Tambak berhasil didapatkan',
+              data: docs
+            })
+          );
+        }
+      );
+    });
+    return;
+  } else if (cityId) {
+    // const isCityIdVal = objectIdValidator(cityId as string, 'User', true);
+    // if (isCityIdVal.error) {
+    //   return res.status(401).send(
+    //     message({
+    //       statusCode: 401,
+    //       data: req.body,
+    //       message: 'City with given Id is not valid!'
+    //     })
+    //   );
+    // }
+
+    const isCityExist = await City.findById(cityId);
+    if (!isCityExist) {
+      return res.status(400).send(
+        message({
+          statusCode: 400,
+          data: req.body,
+          message: 'City with given id is not exist!'
+        })
+      );
+    }
+
+    const ponds = await Ponds.find({
+      pondsName: { $regex: new RegExp(pondsName, 'i') },
+      cityId: cityId,
+      ...NOT_ARCHIVED
+    })
+      .limit(limit || 1 * 1)
+      .skip((page - 1) * limit)
+      .sort('-updatedAt')
+      .exec();
+    if (!ponds.length) {
       return res.status(404).send(
         message({
           statusCode: 404,
           message: 'Tambak tidak ditemukan',
-          data: req.body
+          data: req.query
         })
       );
     }
-    res.send(
+
+    return res.send(
       message({
         statusCode: 200,
-        message: 'Tambak berhasil ditemukan!',
-        data: getAllPonds
-      })
-    );
-  } else {
-    return res.status(404).send(
-      message({
-        statusCode: 404,
-        message: 'User is not found',
-        data: req.body
+        message: 'Tambak berhasil didapatkan',
+        data: ponds
       })
     );
   }
+
+  const ponds = await Ponds.find({ pondsName: { $regex: new RegExp(pondsName, 'i') }, ...NOT_ARCHIVED })
+    .limit(limit || 1 * 1)
+    .skip((page - 1) * limit)
+    .sort('-updatedAt')
+    .exec();
+  if (!ponds.length) {
+    return res.status(404).send(
+      message({
+        statusCode: 404,
+        message: 'Tambak tidak ditemukan',
+        data: req.query
+      })
+    );
+  }
+  return res.send(
+    message({
+      statusCode: 200,
+      message: 'Tambak berhasil didapatkan',
+      data: ponds
+    })
+  );
 }
 
 export { getAllNoQueryPonds };
--- src/controllers/users/get-all.users.ts
+++ src/controllers/users/get-all.users.ts
@@ -7,31 +7,45 @@ import message from '../../views/message';
 async function getAllUserProfiles(req: Request, res: Response) {
   const userId = extractToken(req.headers.authorization, false).result;
   if (userId) {
-    const isUserExist = await Users.find();
-    if (!isUserExist) {
-      return res.status(404).send(
+    try {
+      const users = await Users.find().sort({ createdAt: -1 });
+
+      if (!users.length) {
+        return res.status(404).send(
+          message({
+            statusCode: 404,
+            message: 'User is not found',
+            data: req.body
+          })
+        );
+      }
+
+      return res.send(
         message({
-          statusCode: 404,
-          message: 'User is not found',
-          data: req.body
+          statusCode: 200,
+          message: 'Users retrieved successfully!',
+          data: users
+        })
+      );
+    } catch (error) {
+      console.error('Error:', error);
+      return res.status(500).send(
+        message({
+          statusCode: 500,
+          message: 'Internal Server Error',
+          data: error
         })
       );
     }
-    res.send(
+  } else {
+    return res.status(404).send(
       message({
-        statusCode: 200,
-        message: 'You have successfully updated!',
-        data: isUserExist
+        statusCode: 404,
+        message: 'User is not found',
+        data: req.body
       })
     );
   }
-  res.status(404).send(
-    message({
-      statusCode: 404,
-      message: 'User is not found',
-      data: req.body
-    })
-  );
 }
 
 export { getAllUserProfiles };
